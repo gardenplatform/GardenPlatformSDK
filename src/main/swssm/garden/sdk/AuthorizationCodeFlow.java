16133913c4a38e91fabb65e7c0663003eaa8c50c
SangWoo Chon <ssang1105@live.co.kr>
2014-11-30 15:59:53 +0900

    불필요한 파일을 지운 배포용 SDK

--- src/main/swssm/garden/sdk/AuthorizationCodeFlow.java
+++ src/main/swssm/garden/sdk/AuthorizationCodeFlow.java
@@ -1,6 +1,5 @@
 package swssm.garden.sdk;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -13,10 +12,6 @@ import com.google.api.client.repackaged.com.google.common.base.Preconditions;
 import com.google.api.client.util.Clock;
 import com.google.api.client.util.Joiner;
 import com.google.api.client.util.Lists;
-import com.google.api.client.util.store.DataStore;
-import com.google.api.client.util.store.DataStoreFactory;
-
-//import swssm.garden.sdk.Credential.AccessMethod;
 
 /**
  * 사용자의 Credential을 저장하고 관리하기 위한 Authorization Code Flow
@@ -24,10 +19,6 @@ import com.google.api.client.util.store.DataStoreFactory;
  */
 public class AuthorizationCodeFlow {
 
-//	/**
-//	 * resource server에 알리는 access token의 method (BearerToken)
-//	 */
-//	private final AccessMethod method;
 	/**
 	 * HTTP Transport
 	 */
@@ -52,18 +43,12 @@ public class AuthorizationCodeFlow {
 	private final String approvalPrompt;
 	// /// Authorization Server Encoded URL
 	private final String authorizationServerEncodedUrl;
-	// /// Credentail Data 저장
-//	private final DataStore<StoredCredential> credentialDataStore;
 	// /// HTTP Request Initializer
 	private final HttpRequestInitializer requestInitializer;
 	// /// Credential을 위한 시간
 	private final Clock clock;
 	// /// 범위
 	private final Collection<String> scopes;
-	// /// Credential Created Listener
-//	private final CredentialCreatedListener credentialCreatedListener;
-	// /// 클라이언트에게 제공되는 Refresh Listener
-//	private final Collection<CredentialRefreshListener> refreshListeners;
 
 	public AuthorizationCodeFlow( HttpTransport transport,
 			JsonFactory jsonFactory,HttpExecuteInterceptor clientAuthentication, String clientId) {
@@ -72,7 +57,6 @@ public class AuthorizationCodeFlow {
 	}
 
 	protected AuthorizationCodeFlow(Builder builder) {
-//		method = Preconditions.checkNotNull(builder.method);
 		transport = Preconditions.checkNotNull(builder.transport);
 		jsonFactory = Preconditions.checkNotNull(builder.jsonFactory);
 		tokenServerEncodedUrl = "http://211.189.127.73:8000/o/token/";
@@ -80,12 +64,9 @@ public class AuthorizationCodeFlow {
 		clientId = Preconditions.checkNotNull(builder.clientId);
 		authorizationServerEncodedUrl = "http://211.189.127.73:8000/o/authorize";
 		requestInitializer = builder.requestInitializer;
-//		credentialDataStore = builder.credentialDataStore;
 		scopes = Collections.unmodifiableCollection(builder.scopes);
 		clock = Preconditions.checkNotNull(builder.clock);
-//		credentialCreatedListener = builder.credentialCreatedListener;
 		approvalPrompt = builder.approvalPrompt;
-//		refreshListeners = Collections.unmodifiableCollection(builder.refreshListeners);
 	}
 
 	public AuthorizationCodeRequestUrl newAuthorizationUrl() {
@@ -100,60 +81,6 @@ public class AuthorizationCodeFlow {
 				.setClientAuthentication(clientAuthentication)
 				.setRequestInitializer(requestInitializer).setScopes(scopes);
 	}
-
-//	// /// 주어진 User ID에 따른 새로운 Credential을 생성하는 클래스
-//	@SuppressWarnings("deprecation")
-//	public Credential createAndStoreCredential(TokenResponse response, String clientId) throws IOException {
-//		Credential credential = newCredential(clientId).setFromTokenResponse(response);
-//		if (credentialDataStore != null) {
-//			credentialDataStore.set(clientId, new StoredCredential(credential));
-//		}
-//		if (credentialCreatedListener != null) {
-//			credentialCreatedListener.onCredentialCreated(credential, response);
-//		}
-//		return credential;
-//	}
-//
-//	// /// Client ID를 이용하여 Credential을 Credential이 저장된 곳에서 불러옴. 
-//	@SuppressWarnings("deprecation")
-//	public Credential loadCredential(String clientId) throws IOException {
-//		
-//		if (credentialDataStore == null) {
-//			System.out.println("Credential Store is Empty");
-//			return null;
-//		}
-//		Credential credential = newCredential(clientId);
-//		if (credentialDataStore != null) {
-//			StoredCredential stored = credentialDataStore.get(clientId);
-//			System.out.println("Load Credential from Credential Store for ID : " + stored);
-//			if (stored == null) {
-//				return null;
-//			}
-//			credential.setAccessToken(stored.getAccessToken());
-//			credential.setRefreshToken(stored.getRefreshToken());
-//			credential.setExpirationTimeMilliseconds(stored.getExpirationTimeMilliseconds());
-//		}
-//		return credential;
-//	}
-//
-//	// /// Client ID 기반 새로운 Credential Instance를 만듬. 
-//	@SuppressWarnings("deprecation")
-//	private Credential newCredential(String clientId) {
-//		Credential.Builder builder = new Credential.Builder(method)
-//				.setTransport(transport).setJsonFactory(jsonFactory)
-//				.setTokenServerEncodedUrl("http://211.189.127.73:8000/o/token/")
-//				.setClientAuthentication(clientAuthentication)
-//				.setRequestInitializer(requestInitializer).setClock(clock);
-//		if (credentialDataStore != null) {
-//			builder.addRefreshListener(new DataStoreCredentialRefreshListener(clientId, credentialDataStore));
-//		}
-//		builder.getRefreshListeners().addAll(refreshListeners);
-//		return builder.build();
-//	}
-//
-//	public final AccessMethod getMethod() {
-//		return method;
-//	}
 
 	public final HttpTransport getTransport() {
 		return transport;
@@ -179,10 +106,6 @@ public class AuthorizationCodeFlow {
 		return authorizationServerEncodedUrl;
 	}
 
-//	public final DataStore<StoredCredential> getCredentialDataStore() {
-//		return credentialDataStore;
-//	}
-
 	public final HttpRequestInitializer getRequestInitializer() {
 		return requestInitializer;
 	}
@@ -203,20 +126,7 @@ public class AuthorizationCodeFlow {
 		return approvalPrompt;
 	}
 
-//	public final Collection<CredentialRefreshListener> getRefreshListeners() {
-//		return refreshListeners;
-//	}
-
-	// /// Typical use is to parse additional fields from the credential
-	// created, such as an ID token.
-//	public interface CredentialCreatedListener {
-//		void onCredentialCreated(Credential credential,
-//				TokenResponse tokenResponse) throws IOException;
-//	}
-
 	public static class Builder {
-
-//		AccessMethod method;
 
 		HttpTransport transport;
 
@@ -232,18 +142,11 @@ public class AuthorizationCodeFlow {
 
 		String authorizationServerEncodedUrl;
 
-//		DataStore<StoredCredential> credentialDataStore;
-
 		HttpRequestInitializer requestInitializer;
 
 		Collection<String> scopes = Lists.newArrayList();
 
 		Clock clock = Clock.SYSTEM;
-
-//		CredentialCreatedListener credentialCreatedListener;
-//
-//		Collection<CredentialRefreshListener> refreshListeners = Lists
-//				.newArrayList();
 
 		public Builder(HttpTransport transport,
 				JsonFactory jsonFactory,HttpExecuteInterceptor clientAuthentication, String clientId) {
@@ -260,15 +163,6 @@ public class AuthorizationCodeFlow {
 		public AuthorizationCodeFlow build() {
 			return new AuthorizationCodeFlow(this);
 		}
-
-//		public final AccessMethod getMethod() {
-//			return method;
-//		}
-//
-//		public Builder setMethod(AccessMethod method) {
-//			this.method = Preconditions.checkNotNull(method);
-//			return this;
-//		}
 
 		public final HttpTransport getTransport() {
 			return transport;
@@ -327,10 +221,6 @@ public class AuthorizationCodeFlow {
 			return this;
 		}
 
-//		public final DataStore<StoredCredential> getCredentialDataStore() {
-//			return credentialDataStore;
-//		}
-
 		
 		public final Clock getClock() {
 			return clock;
@@ -340,16 +230,6 @@ public class AuthorizationCodeFlow {
 			this.clock = Preconditions.checkNotNull(clock);
 			return this;
 		}
-
-//		public Builder setDataStoreFactory(DataStoreFactory dataStoreFactory)
-//				throws IOException {
-//			return setCredentialDataStore(StoredCredential.getDefaultDataStore(dataStoreFactory));
-//		}
-//
-//		public Builder setCredentialDataStore(	DataStore<StoredCredential> credentialDataStore) {
-//			this.credentialDataStore = credentialDataStore;
-//			return this;
-//		}
 
 		public final HttpRequestInitializer getRequestInitializer() {
 			return requestInitializer;
@@ -378,31 +258,6 @@ public class AuthorizationCodeFlow {
 		public final String getApprovalPrompt() {
 			return approvalPrompt;
 		}
-
-//		public Builder setCredentialCreatedListener(CredentialCreatedListener credentialCreatedListener) {
-//			this.credentialCreatedListener = credentialCreatedListener;
-//			return this;
-//		}
-//
-//		public Builder addRefreshListener(
-//				CredentialRefreshListener refreshListener) {
-//			refreshListeners.add(Preconditions.checkNotNull(refreshListener));
-//			return this;
-//		}
-//
-//		public final Collection<CredentialRefreshListener> getRefreshListeners() {
-//			return refreshListeners;
-//		}
-//
-//		public Builder setRefreshListeners(Collection<CredentialRefreshListener> refreshListeners) {
-//			this.refreshListeners = Preconditions
-//					.checkNotNull(refreshListeners);
-//			return this;
-//		}
-//
-//		public final CredentialCreatedListener getCredentialCreatedListener() {
-//			return credentialCreatedListener;
-//		}
 
 	}
 
